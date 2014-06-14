6c8ccf9c928dae42b599e9f3c55af4533b12a8ed
Trent Willis <trentmwillis@gmail.com>
2014-06-13 23:49:47 -0400

    Core gameplay is finished. Next: refine gameplay and styling

--- js/app.js
+++ js/app.js
@@ -10,8 +10,14 @@ var Sudoku = typeof Sudoku != 'undefined' ? Sudoku : {};
 
         // Determine which numbers are available for the spot
         var usedNumbers = [],
-            $section = $lastCellClicked.parents('.board-section').first(),
-            $cells = $section.find('.cell');
+            $row = $lastCellClicked.parent('tr'),
+            $section = $row.parents('.board-section').first(),
+            $rowGroup = $section.parent('.row-group'),
+            rowNum = ($row.index() + 1) + (3 * $rowGroup.index()),
+            colNum = ($lastCellClicked.index() + 1) + (3 * $section.index()),
+            $cells = $section.find('.cell'),
+            $cells = $cells.add('.row-' + rowNum + ' > .cell'),
+            $cells = $cells.add('.board-section:nth-child(' + ($section.index()+1) + ') .cell:nth-child(' + ($lastCellClicked.index()+1) + ')');
 
         $cells.forEach(function(cell) {
             if ($(cell).text()) {
@@ -36,6 +42,21 @@ var Sudoku = typeof Sudoku != 'undefined' ? Sudoku : {};
 
         $lastCellClicked.text($cell.text());
         $('#number-grid').addClass('hide');
+        Sudoku.validate();
     });
+
+    Sudoku.validate = function() {
+        var valid = true;
+
+        $('.cell').forEach(function(cell) {
+            if (!$(cell).text()) {
+                valid = false;
+            }
+        });
+
+        if (valid) {
+            alert("Yay!");
+        }
+    };
 
 })();
